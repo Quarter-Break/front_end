50567778f2f250254963d3d24889fe5ea9a725c3
Sverre van Gompel <403979@student.fontys.nl>
2021-04-14 15:53:06 +0200

    Removed node folder

---
+++ src/pages/Settings.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <h1 class="text-h2">Settings</h1>
+        <v-btn v-on:click="test">Test</v-btn>
+    </v-container>
+</template>
+
+<script>
+    import trackservice from "../javascript/axios/tracks/streaming_service";
+
+    export default {
+        name: "Settings",
+        methods: {
+            test() {
+                const id = "6061c90b2658b9001e65311d";
+
+                trackservice.getTrack(id);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
